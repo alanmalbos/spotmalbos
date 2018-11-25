b603228643ef464bf860f85d5e4fa551b56617a9
Alan Malbos <alanmalbos@gmail.com>
2018-11-25 18:58:08 -0200

    Create a Dashboard Controller and View. Make some alterations on design buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  root to: 'home#index'
+  
+  #create a root with autorization needed
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  #root for unauthenticated users
+  unauthenticated :user do
+    root to: "home#index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
